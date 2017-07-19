e0ffbc7c35ef51e3b56ffeeb1d78336c2a01eace
jaybz <jaybz.e@gmail.com>
2017-07-19 17:33:17 +0800

    Delete unnecessary  drawable xml

--- PogoniumImporter/PogoniumImporter.Android/Resources/Resource.designer.cs
+++ PogoniumImporter/PogoniumImporter.Android/Resources/Resource.designer.cs
@@ -2809,8 +2809,26 @@ namespace PogoniumImporter.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
+			// aapt resource value: 0x7f020069
+			public const int avd_hide_password_1 = 2130837609;
+			
+			// aapt resource value: 0x7f02006a
+			public const int avd_hide_password_2 = 2130837610;
+			
+			// aapt resource value: 0x7f02006b
+			public const int avd_hide_password_3 = 2130837611;
+			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
+			
+			// aapt resource value: 0x7f02006c
+			public const int avd_show_password_1 = 2130837612;
+			
+			// aapt resource value: 0x7f02006d
+			public const int avd_show_password_2 = 2130837613;
+			
+			// aapt resource value: 0x7f02006e
+			public const int avd_show_password_3 = 2130837614;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2860,20 +2878,17 @@ namespace PogoniumImporter.Droid
 			// aapt resource value: 0x7f020064
 			public const int notification_icon_background = 2130837604;
 			
-			// aapt resource value: 0x7f020068
-			public const int notification_template_icon_bg = 2130837608;
+			// aapt resource value: 0x7f020067
+			public const int notification_template_icon_bg = 2130837607;
 			
-			// aapt resource value: 0x7f020069
-			public const int notification_template_icon_low_bg = 2130837609;
+			// aapt resource value: 0x7f020068
+			public const int notification_template_icon_low_bg = 2130837608;
 			
 			// aapt resource value: 0x7f020065
 			public const int notification_tile_bg = 2130837605;
 			
 			// aapt resource value: 0x7f020066
 			public const int notify_panel_notification_icon_bg = 2130837606;
-			
-			// aapt resource value: 0x7f020067
-			public const int skin_iv_linearlayout = 2130837607;
 			
 			static Drawable()
 			{
